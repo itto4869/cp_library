a247bee8ce46c04f24c0df072a1022bf4ea59a03
itto4869 <itto4869@gmail.com>
2026-04-05 02:16:39 +0900

    Add fastout-safe yes_no macros

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,9 +1,29 @@
-pub fn yes_no(b: bool) {
-    yes_no_custom(b, "Yes", "No");
+#[macro_export]
+macro_rules! yes_no {
+    ($b:expr) => {
+        $crate::yes_no_custom!($b, "Yes", "No")
+    };
 }
 
-pub fn yes_no_custom(b: bool, yes: &str, no: &str) {
-    println!("{}", if b { yes } else { no });
+#[macro_export]
+macro_rules! yes_no_custom {
+    ($b:expr, $yes:expr, $no:expr) => {
+        println!("{}", if $b { $yes } else { $no })
+    };
+}
+
+#[must_use]
+pub const fn yes_no(b: bool) -> &'static str {
+    yes_no_custom(b, "Yes", "No")
+}
+
+#[must_use]
+pub const fn yes_no_custom<'a>(b: bool, yes: &'a str, no: &'a str) -> &'a str {
+    if b {
+        yes
+    } else {
+        no
+    }
 }
 
 #[cfg(test)]
@@ -12,13 +32,25 @@ mod tests {
 
     #[test]
     fn test_yes_no() {
-        yes_no(true);
-        yes_no(false);
+        assert_eq!(yes_no(true), "Yes");
+        assert_eq!(yes_no(false), "No");
     }
 
     #[test]
     fn test_yes_no_custom() {
-        yes_no_custom(true, "YES", "NO");
-        yes_no_custom(false, "Possible", "Impossible");
+        assert_eq!(yes_no_custom(true, "YES", "NO"), "YES");
+        assert_eq!(yes_no_custom(false, "Possible", "Impossible"), "Impossible");
+    }
+
+    #[test]
+    fn test_yes_no_macro() {
+        crate::yes_no!(true);
+        crate::yes_no!(false);
+    }
+
+    #[test]
+    fn test_yes_no_custom_macro() {
+        crate::yes_no_custom!(true, "YES", "NO");
+        crate::yes_no_custom!(false, "Possible", "Impossible");
     }
 }
